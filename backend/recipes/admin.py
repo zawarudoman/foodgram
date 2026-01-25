3c00908bd774ddc963e056f4f2b101ac9de224d9
Максим Лесков <lml@MacBook-Air-Maksim.local>
2026-01-25 19:33:50 +0300

    setting recipes admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'cooking_time', 'pub_date')
+    search_fields = ('name', 'author__username')
+    list_filter = ('tags', 'pub_date')
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
