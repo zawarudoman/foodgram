5b9118f31b0d4f8868820cbe00d1470f9a330d26
Максим Лесков <lml@MacBook-Air-Maksim.local>
2026-01-25 15:19:40 +0300

    add standart model Recipe

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Recipe(models.Model):
+    name = models.CharField('Название', max_length=200)
+    text = models.TextField('Описание')
+    image = models.ImageField('Изображение', upload_to='recipes/')
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
